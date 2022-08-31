51b9e1a37ebd0c4ab4d13de3ac3047fb2310b7fa
pony <mspony@163.com>
2022-08-31 14:18:00 +0800

    tmp

--- src/pf_arith/draft/bench.c
+++ src/pf_arith/draft/bench.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
 #include "clock.h"
+#include <x86intrin.h>
 void mont_mul_opt(unsigned long **res);
 unsigned long* mul(unsigned long A[10],unsigned long B[10]);
+int mul_avx2(__m256i E_F[5], __m256i A_B[5], __m256i C_D[5]);
 
 extern unsigned long A[10],B[10];
 
 int main()
 {
-    const int BENCH = 800;
+    const int BENCH = 1000;
     unsigned long *res;
 
+    __m256i A_B[5];
+    __m256i C_D[5];
+    __m256i E_F[5] = {0};
+    A_B[0] = _mm256_set_epi64x(15645338, 11397043, 56030310, 51423524);
+    A_B[1] = _mm256_set_epi64x(45096011, 48583030, 62398102, 25976630);
+    A_B[2] = _mm256_set_epi64x(26780030, 19015266, 51347875, 29954667);
+    A_B[3] = _mm256_set_epi64x(5688755, 27671603, 54947324, 39376672);
+    A_B[4] = _mm256_set_epi64x(214373, 12582839, 2041311, 16914210);
+
+    C_D[0] = _mm256_set_epi64x(35879724, 26788201, 61994887, 24198817);
+    C_D[1] = _mm256_set_epi64x(48789969, 24411185, 930661, 10215065);
+    C_D[2] = _mm256_set_epi64x(20757944, 16939434, 17935313, 12818618);
+    C_D[3] = _mm256_set_epi64x(64360620, 43993867, 8017293, 28815027);
+    C_D[4] = _mm256_set_epi64x(3961645, 27684150, 911207, 45174287);
+
     // CLOCKS(MontMul,mont_mul_opt(&res));
-    CLOCKS(MontMul,mul(A,B));
+    CLOCKS(MontMul,mul_avx2(E_F, A_B, C_D));
+
 
 }
--- src/pf_arith/draft/mont_mul.py
+++ src/pf_arith/draft/mont_mul.py
@@ -1,14 +1,7 @@
-import math
-
-
-T=[0x155f045, 0x1e9dc40, 0x37ea842, 0x12eb2f0, 0x25b36cb,
-                       0x216bd51, 0x2faefcb, 0x224621e, 0x3791fa7, 0x3d47300,
-                       0x2bf0f7,  0x3cc7bae, 0x5f9f35,  0x38363d9, 0x379adcf,
-                       0x2b58b39, 0x910a46,  0x466919,  0x21ecf1c, 0x2074]
-N=2^256-2^224-2^96+2^64-1 # p_sm2
-R_inv=115792089102516462125480396441826910452883803264411231995480180208556130172932
-N_1=0x4fffffffc00000001fffffffe00000000ffffffff000000010000000000000001
-R=2**260
+"""
+乘法+约减
+"""
+import random
 
 def list2Int(a_list,radix):
     a=0
@@ -25,40 +18,82 @@ def int2list(a,radix):
         # print(a)
     return a_list
 
-Tx_values=[]
-Tx_1_values=[]
-tx_values=[]
+A_bn=random.randint(2**253,2**256-1)
+B_bn=random.randint(2**253,2**256-1)
 
-Tx_values.append("T2=t2&(2**26-1)")
-Tx_1_values.append("T3_1=t3+(t2>>26)")
+A=int2list(A_bn,2**26)
+B=int2list(B_bn,2**26)
+
+N=2^256-2^224-2^96+2^64-1 # p_sm2
+R_inv=115792089102516462125480396441826910452883803264411231995480180208556130172932
+N_1=0x4fffffffc00000001fffffffe00000000ffffffff000000010000000000000001
+R=2**260
+
+T_1x_values=['']*19  # T=A*B的临时digits
+tx_values=[]    # T约减后的digits
+Tx_1_values=[]  # T约减后并完成carry prop
+Tx_values=[]    # T=A*B T约减后并压缩后的digits
+
+i=0
+while i<10:
+    j=0
+    while j<10:
+        if T_1x_values[i+j]=='':
+            T_1x_values[i+j]="T_1[{}]=A[{}]*B[{}]+".format(i+j,i,j)
+        else:
+            T_1x_values[i+j]+="A[{}]*B[{}]+".format(i,j)
+        j+=1
+
+    i+=1
+i=0
+while i<19:
+    T_1x_values[i]=T_1x_values[i][:-1]
+    i+=1
+        
+# print(T_1x_values)
+
+Tx_1_values.append("T1_1=t1+(t0>>26)")
+Tx_values.append("T0=t0&(2**26-1)")
+
 j=0
-while j<20:
-    tx_values.append("t{}=T[{}]".format(j,j))
+while j<19:
+    tx_values.append("t{}=T_1[{}]".format(j,j))
 
-    if j>2 and j<10:
-        Tx_values.append("T{}=T{}_1&(2**26-1)".format(j,j))
-
-    if j>3 and j<10:
+    if j>1 and j<10:
         Tx_1_values.append("T{}_1=t{}+(T{}_1>>26)".format(j,j,j-1))
+
+    if j>0 and j<10:
+        Tx_values.append("T{}=T{}_1&(2**26-1)".format(j,j))
 
     j+=1
 
 i=0
 while i<10:
-
-    if i<2:
-        tx_values[i+2]+="+(t{}<<12)".format(i)
-        tx_values[i+3]+="-(t{}<<18)".format(i)
-        tx_values[i+8]+="-(t{}<<16)".format(i)
-        tx_values[i+9]+="+(t{}<<22)".format(i)
-    else:
-        tx_values[i+2]+="+(T{}<<12)".format(i)
-        tx_values[i+3]+="-(T{}<<18)".format(i)
-        tx_values[i+8]+="-(T{}<<16)".format(i)
-        tx_values[i+9]+="+(T{}<<22)".format(i)
+    tx_values[i+2]+="+(T{}<<12)".format(i)
+    tx_values[i+3]+="-(T{}<<18)".format(i)
+    tx_values[i+8]+="-(T{}<<16)".format(i)
+    tx_values[i+9]+="+(T{}<<22)".format(i)
 
     i+=1
 
-print(Tx_1_values)
-print(Tx_values)
-print(tx_values)
+# print(Tx_1_values)
+# print(Tx_values)
+# print(tx_values)
+# print(len(T_1x_values),len(Tx_1_values),len(Tx_values),len(tx_values))
+
+def printCode():
+    i=0
+    while i<19:
+        if i<10:
+            print(T_1x_values[i])
+            print(tx_values[i])
+            if i>0:
+                print(Tx_1_values[i-1])
+            print(Tx_values[i])
+
+        else:
+            print(T_1x_values[i])
+            print(tx_values[i])
+        i+=1
+
+printCode()
